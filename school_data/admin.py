ddbbbbf3a8a610de31f5746cc3087bfc79a46941
pselle <pamela.selle@gmail.com>
2013-02-13 17:11:33 -0500

    Admin

---
+++ school_data/admin.py
@@ -0,0 +1,5 @@
+from school_data.models import School, Address
+from django.contrib import admin
+
+admin.site.register(School)
+admin.site.register(Address)
--- sdp_curricula/settings/base.py
+++ sdp_curricula/settings/base.py
@@ -137,7 +137,7 @@ INSTALLED_APPS = (
     'debug_toolbar',
 
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
