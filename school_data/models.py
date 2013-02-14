7ff3a19dfa7d96d13ef2a4edcc8161c40a83e229
pselle <pamela.selle@gmail.com>
2013-02-14 15:57:05 -0500

    Update models

--- school_data/models.py
+++ school_data/models.py
@@ -24,7 +24,7 @@ class Grade(models.Model):
 
 class Cohort(models.Model):
     YEARS = []
-    for r in range(2008, (datetime.datetime.now().year+1)):
+    for r in range(2008, (datetime.now().year+1)):
         YEARS.append((r,r))
 
     grade = models.ForeignKey(Grade)
@@ -41,4 +41,37 @@ class Cohort(models.Model):
     READ_BASIC_PERCENT = models.IntegerField()
     READ_BELOW_BASIC_PERCENT = models.IntegerField()
     MATH_COMBINED_PERCENT = models.IntegerField()
-    READ_COMBINED_PERCENT = models.IntegerField() 
+    READ_COMBINED_PERCENT = models.IntegerField()
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+class Textbook(models.Model):
+    isbn = models.CharField(max_length=10)
+    title = models.CharField(max_length=200)
+    publisher = models.ForeignKey(Publisher)
+    grade_level_start = models.IntegerField()
+    grade_level_end = models.IntegerField()
+
+class InventoryRecord(models.Model):
+    school = models.ForeignKey(School)
+    textbook = models.ForeignKey(Textbook)
+    qty_onsite = models.IntegerField()
+    qty_to_student_home = models.IntegerField()
+    qty_to_student_class = models.IntegerField()
+    qty_lost_stolen = models.IntegerField()
+    qty_unusable = models.IntegerField()
+    qty_reallocated = models.IntegerField()
+
+class Curriculum(models.Model):
+    SUBJECTS = [('Reading','Reading'),
+                ('Mathematics','Mathematics'),
+                ('Language Arts','Language Arts'),
+                ('Science','Science'),
+                ('Social Studies', 'Social Studies')]
+    books = models.ManyToManyField(Textbook)
+    publisher = models.ForeignKey(Publisher)
+    grade_level_start = models.IntegerField()
+    grade_level_end = models.IntegerField()
+    is_empowerment = models.BooleanField(default=False)
+    subject_area = models.CharField(max_length=100, choices=SUBJECTS)
