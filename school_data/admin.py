26ba84a093fcf4b5026f67264ab1618678297a41
Jeff <jfinbox@comcast.net>
2013-02-20 12:00:42 -0500

    Populate Grade table and add grade to administrative area.

--- school_data/admin.py
+++ school_data/admin.py
@@ -1,4 +1,10 @@
-from school_data.models import School
+from school_data.models import School, Grade
 from django.contrib import admin
 
 admin.site.register(School)
+
+
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('school', 'grade_level')
+
+admin.site.register(Grade, GradeAdmin)
