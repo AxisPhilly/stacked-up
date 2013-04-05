a19f6b3b4a8326f6581e468c580adbabdec64337
Jeff Frankl <jfinbox@comcast.net>
2013-04-05 08:32:10 -0400

    I accidentally removed this prefetch in my last commit.

--- students/admin.py
+++ students/admin.py
@@ -11,12 +11,15 @@ admin.site.register(Grade, GradeAdmin)
 
 class CohortAdmin(admin.ModelAdmin):
 
+    def queryset(self, request):
+        return Cohort.objects.all().prefetch_related('grade')
+
     readonly_fields = ('grade',)
     list_display = ('grade', 'year_start', 'year_end',
-        'math_advanced_percent', 'math_proficient_percent',
-        'math_basic_percent', 'math_below_basic_percent',
-        'read_advanced_percent', 'read_proficient_percent',
-        'read_basic_percent', 'read_below_basic_percent',
-        'math_combined_percent')
+                    'math_advanced_percent', 'math_proficient_percent',
+                    'math_basic_percent', 'math_below_basic_percent',
+                    'read_advanced_percent', 'read_proficient_percent',
+                    'read_basic_percent', 'read_below_basic_percent',
+                    'math_combined_percent')
 
 admin.site.register(Cohort, CohortAdmin)
