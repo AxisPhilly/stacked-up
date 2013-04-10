97a4d58501e07a94c5ce714e3aa9b3271e1ae878
pselle <pamela.selle@gmail.com>
2013-04-10 15:11:52 -0400

    Improve UI

--- schools/admin.py
+++ schools/admin.py
@@ -3,6 +3,14 @@ from .models import District, SchoolType, School
 
 admin.site.register(District)
 
-admin.site.register(SchoolType)
 
-admin.site.register(School)
+class SchoolTypeAdmin(admin.ModelAdmin):
+    filter_horizontal = ['approved_curricula']
+
+admin.site.register(SchoolType, SchoolTypeAdmin)
+
+
+class SchoolAdmin(admin.ModelAdmin):
+    filter_horizontal = ['curricula_in_use']
+
+admin.site.register(School, SchoolAdmin)
