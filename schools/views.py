2953739b1c625400e717282515b65ef799835273
Jeff <jfinbox@comcast.net>
2013-04-11 16:16:38 -0400

    School ids are now school codes. Updated templates/views to reflect this.

--- schools/views.py
+++ schools/views.py
@@ -9,7 +9,7 @@ class SchoolCurriculaMatch(ListView):
     template_name = "school_curricula_match.html"
 
     def get_queryset(self):
-        self.school = School.objects.get(school_id=self.kwargs['id'])
+        self.school = School.objects.get(school_code=self.kwargs['id'])
         each_book = InventoryRecord.objects.filter(school=self.school).prefetch_related('material__publisher__group')
         return each_book
 
@@ -20,7 +20,7 @@ class SchoolCurriculaMatch(ListView):
         unmatched_books = {}
         matched_curricula = {}
         for book in all_books:
-            match_list = book.material.curricula.all().prefetch_related('curriculum__publisher')
+            match_list = book.material.curricula.all()
             title = book.material.title
             isbn = book.material.isbn
             teacher_edition = book.material.isTeacherEdition
@@ -52,7 +52,7 @@ class SchoolInventory(ListView):
     template_name = "school_inventory.html"
 
     def get_queryset(self):
-        self.school = School.objects.get(school_id=self.kwargs['id'])
+        self.school = School.objects.get(school_code=self.kwargs['id'])
         each_book = InventoryRecord.objects.filter(school=self.school).prefetch_related('material__publisher__group').order_by('material')
         return each_book
 
@@ -68,7 +68,7 @@ class SchoolDetailView(ListView):
     template_name = "school.html"
 
     def get_queryset(self):
-        self.school = School.objects.get(school_id=self.kwargs['id'])
+        self.school = School.objects.get(school_code=self.kwargs['id'])
         each_book = InventoryRecord.objects.filter(school=self.school)
         return each_book
 
