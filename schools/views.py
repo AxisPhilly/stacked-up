20db92560f133fe802813aac0e2084b9d942c74e
Jeff <jfinbox@comcast.net>
2013-04-25 17:50:30 -0400

    Import test data.

--- schools/views.py
+++ schools/views.py
@@ -1,6 +1,7 @@
 from .models import School
-from vendors.models import InventoryRecord
+from vendors.models import InventoryRecord, NegotiatedPrice
 from curricula.models import GradeCurriculum
+from students.models import Grade, Cohort
 from django.views.generic import ListView
 import simplejson as json
 
@@ -97,32 +98,46 @@ class SchoolAggregateView(ListView):
         all_books = InventoryRecord.objects.select_related('material').filter(school=self.school)
         return all_books
 
-    def get_materials_for_grade_curriculum(self, grade_curriculum, array_name):
+    def enough_books(self, number_of_books, students_in_grade):
+        if number_of_books >= students_in_grade:
+            return('True')
+        else:
+            return('False')
+
+    def get_materials_for_grade_curriculum(self, students_in_grade, array_name, all_books, grade_curriculum_id):
         self.book_list[array_name] = []
-        for material in grade_curriculum.all():
+        required_books = GradeCurriculum.objects.get(id=grade_curriculum_id).materials  # place .materials with .necessary_materials to get only necessary material when this data has been added
+        for material in required_books.all():
+            if all_books.filter(material=material).exists():
+                number_of_books = all_books.filter(material=material)[0].get_inventory_total()
+            else:
+                number_of_books = 0
+            enough_books = self.enough_books(number_of_books, number_of_books)
+            cost_of_book = NegotiatedPrice.objects.filter(material=material)[0].value
             self.book_list[array_name].append(
                 {
                     'title': material.title,
-                    'isbn': material.isbn,
+                    'total copies': number_of_books,
+                    'needed': students_in_grade,
+                    'cost': float(cost_of_book),
+                    'enough': enough_books,
+                    'difference': abs(students_in_grade - number_of_books),
                 })
+            if (students_in_grade - number_of_books) >= 0:
+                self.total_cost += (students_in_grade - number_of_books) * cost_of_book
+            else:
+                self.total_cost += 0
 
     def get_context_data(self, **kwargs):
         context = super(SchoolAggregateView, self).get_context_data(**kwargs)
-        context['school'] = self.school.name
-        context['grade_start'] = self.school.grade_start
-        context['grade_end'] = self.school.grade_end
-        context['curricula_in_use'] = self.school.curricula_in_use
+        self.total_cost = 0
+        context['school'] = self.school
+        grade = Grade.objects.get(school=self.school, grade_level=5)
+        cohort = Cohort.objects.filter(grade=grade)
+        students_in_grade = cohort.get(grade=grade, year_end=2013).number_of_students
         self.book_list = {}
-        self.book_list['all_books'] = []
-        gc = GradeCurriculum.objects.filter(id=86)[0].necessary_materials
-        self.get_materials_for_grade_curriculum(gc, 'reading_books')
-        self.get_materials_for_grade_curriculum(gc, 'mathematics_books')
-        for material in context['curricula']:
-            self.book_list['all_books'].append(
-                {
-                    'title': material.material.title,
-                    'isbn': material.material.isbn,
-                    'quantity': material.get_inventory_total(),
-                })
-        context['book_list'] = json.dumps(self.book_list)
+        self.get_materials_for_grade_curriculum(students_in_grade, 'reading', context['curricula'], 38)
+        self.get_materials_for_grade_curriculum(students_in_grade, 'mathematics', context['curricula'], 30)
+        context['book_list'] = self.book_list
+        context['outtwo'] = json.dumps([obj for obj in cohort.values()])
         return context
