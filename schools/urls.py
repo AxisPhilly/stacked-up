9c03a8bf4b80d95851c131f1d453b439fcd75b5f
Jeff <jfinbox@comcast.net>
2013-04-30 17:07:12 -0400

    Move schools urls to schools.urls.

--- schools/urls.py
+++ schools/urls.py
@@ -4,6 +4,8 @@ from .views import SchoolsListView, SchoolAggregateView
 urlpatterns = patterns('',
     url(r'^$',
         SchoolsListView.as_view()),
+    url(r'^test/(?P<id>(.+))/(?P<grade>(.+))/$',
+        SchoolAggregateView.as_view()),
     url(r'^test/(?P<id>(.+))/$',
         SchoolAggregateView.as_view()),
 )
