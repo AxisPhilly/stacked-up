5cea5cd47cf1efccadc5e3ecd7954f7bd6d938e5
pselle <pamela.selle@gmail.com>
2013-05-06 15:50:11 -0400

    Get instead of filter for records

--- schools/views.py
+++ schools/views.py
@@ -114,11 +114,7 @@ class SchoolAggregateView(ListView):
         self.curriculum_list[subject]['curricula'][grade_curriculum_name]['necessary_material'] = []
         for material in grade_curriculum.necessary_materials.all():
             if all_books.filter(material=material).exists():
-                """
-                TODO: change all_books.filter(material=material)[0].get_inventory_total() to
                 number_of_books = all_books.get(material=material).get_inventory_total()
-                """
-                number_of_books = all_books.filter(material=material)[0].get_inventory_total()
                 enough_books = self.is_enough_books(number_of_books, students_in_grade)
                 cost_of_book = NegotiatedPrice.objects.filter(material=material)[0].value
                 difference = number_of_books - students_in_grade
