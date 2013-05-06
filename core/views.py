57d981220a2e66edd7e55769a0a7fc70480da560
pselle <pamela.selle@gmail.com>
2013-05-06 16:42:11 -0400

    Change splicing

--- core/views.py
+++ core/views.py
@@ -10,6 +10,6 @@ class IndexListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(IndexListView, self).get_context_data(**kwargs)
         filtered = sorted(SchoolAggregate.objects.all(), key=lambda a: a.books_for_students)
-        context['top_schools'] = filtered[:5]
-        context['bottom_schools'] = filtered[-5:]
+        context['top_schools'] = filtered[-5:]
+        context['bottom_schools'] = filtered[:5]
         return context
