a3918e2ca673d59d1ba7b3394ce32b56d30133e6
pselle <pamela.selle@gmail.com>
2013-05-13 11:24:02 -0400

    move log up

--- staticfiles/js/site.js
+++ staticfiles/js/site.js
@@ -23,11 +23,9 @@ app.getChartData = function(school_pk, grade) {
       url: '/static/data/pssa.json'
     })
   ).then(function(schoolInfo, districtInfo) {
+    console.log(districtInfo);
     var schoolScores = app.formatPSSASchoolData(JSON.parse(schoolInfo[0].curriculum.pssa_test_scores));
     var districtScores = JSON.parse(districtInfo[0][grade]);
-    console.log(schoolScores);
-    console.log(districtScores);
-    console.log(districtInfo);
     if(schoolScores.read.length > 1) {
       app.createChart(schoolScores, districtScores, 'read', 'reading-scores');
       app.createChart(schoolScores, districtScores, 'math', 'math-scores');
